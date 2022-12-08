13a12dda01a5e4ecca180fdb2e70b70c3e92eb28
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-12-08 13:58:30 -0300

    :recycle: feat: primary errors

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,32 +1,55 @@
 const UsersModel = require("../models/usersModel");
 
 const allUsersService = async () => {
-  const users = await UsersModel.find();
-  return users;
+  try {
+    const users = await UsersModel.find();
+    return users;
+  } catch (err) {
+    return err;
+  }
 };
 
 const userByIdService = async (id) => {
-  const user = await UsersModel.findById(id);
-  return user;
+  try {
+    const user = await UsersModel.findById(id);
+    return user;
+  } catch (err) {
+    return "Usuário não encontrado!";
+  }
 };
 
 const createUserService = async (name, email, pass) => {
-  const createUser = await UsersModel.create({ name, email, pass });
-  return createUser;
+  try {
+    if (await UsersModel.findOne({ email })) {
+      return "Usuário já existe!";
+    }
+    const createUser = await UsersModel.create({ name, email, pass });
+    return createUser;
+  } catch (err) {
+    return err;
+  }
 };
 
 const deleteUserService = async (id) => {
-  const userDeleted = await UsersModel.findByIdAndDelete(id);
-  return userDeleted;
+  try {
+    const userDeleted = await UsersModel.findByIdAndDelete(id);
+    return userDeleted;
+  } catch (err) {
+    return err;
+  }
 };
 
 const editUserService = async (id, name, email, pass) => {
-  const userEdited = await UsersModel.findByIdAndUpdate(id, {
-    name,
-    email,
-    pass,
-  });
-  return userEdited;
+  try {
+    const userEdited = await UsersModel.findByIdAndUpdate(id, {
+      name,
+      email,
+      pass,
+    });
+    return userEdited;
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = {
